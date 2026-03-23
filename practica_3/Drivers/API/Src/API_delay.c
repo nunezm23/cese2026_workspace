47250cd99533d088cbb6f4a4145d0e87cf4bc48b
Marcos Nuñez <mnnunezc23@gmail.com>
2026-03-22 23:06:10 -0500

    ADD: code for practice 3

---
+++ practica_3/Drivers/API/Src/API_delay.c
@@ -0,0 +1,95 @@
+/**
+  ******************************************************************************
+  * @file           : API_delay.c
+  * @brief          : Delay functions implementations for API.
+  * @author			: Marcos Nuñez
+  * @version		: 1.0.0
+  ******************************************************************************
+*/
+
+/** Section for defining libraries, variables */
+
+/**
+ * @brief Including header file for API implementation
+ * */
+#include "API_delay.h"
+
+/**
+ *@brief Standard C library for including types of variables in the implementation.
+ * */
+#include <stdio.h>
+
+/**
+ * @brief HAL library for use ticks functions related to STM32F4xx devices.
+ * */
+#include "stm32f4xx_hal.h"
+
+/**
+ * @brief Invalid tick parameter to set in delayWrite function when the duration is 0.
+ * */
+#define INVALID_TICK_PARAMETER 0
+
+/**
+  * @brief Number of half-cycles to change state in LED for 50% duty cycle.
+  */
+#define NBR_HALF_CYCLES		10
+
+/** Public functions implementations */
+
+void delayInit( delay_t * delay, tick_t duration )
+{
+	if(NULL == delay || INVALID_TICK_PARAMETER == duration)
+	{
+		return;
+	}
+	delay->duration = duration;
+	delay->running = false;
+	return;
+}
+
+bool_t delayRead( delay_t * delay )
+{
+	bool_t bool_ret = false;
+	if(NULL == delay)
+	{
+		return bool_ret;
+	}
+	if(false == delay->running)
+	{
+		delay->startTime = HAL_GetTick();
+		delay->running = true;
+		return bool_ret;
+	}else if(true == delay->running)
+	{
+		uint32_t current_time = HAL_GetTick();;
+		uint32_t start_time = delay->startTime;
+		if( (current_time - start_time) >= delay->duration)
+		{
+			delay->running = false;
+			bool_ret = true;
+			return bool_ret;
+		}
+	}
+	return bool_ret;
+}
+
+void delayWrite( delay_t * delay, tick_t duration )
+{
+	if(NULL == delay || INVALID_TICK_PARAMETER == duration)
+	{
+		return;
+	}
+	delay->duration = duration;
+	return;
+}
+
+bool_t delayIsRunning(delay_t * delay)
+{
+	bool_t b_ret = true;
+	if(NULL == delay)
+	{
+		return b_ret;
+	}
+	b_ret = delay->running;
+	return b_ret;
+}
