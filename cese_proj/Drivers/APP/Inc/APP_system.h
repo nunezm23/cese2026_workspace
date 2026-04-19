c170214a440fb6a2ec03e16d0ae27c616d84c3d4
Marcos Nuñez <mnnunezc23@gmail.com>
2026-04-19 11:51:43 -0500

    ADD: lcd and mcp init from system app

--- cese_proj/Drivers/APP/Inc/APP_system.h
+++ cese_proj/Drivers/APP/Inc/APP_system.h
@@ -7,3 +7,23 @@
   * @date           : 2024-04-18
   ******************************************************************************
 */
+
+#ifndef __APP_system_H /**< To prevent recursive inclusion*/
+#define __APP_system_H
+
+#include <stdint.h>
+
+#define SYSTEM_RET		int8_t
+
+typedef enum
+{
+	SYS_OK = 0,
+	SYS_ERR_INIT,
+	SYS_ERR_NULL_POINTER,
+	SYS_ERR_INVALID_PARAMS,
+	SYS_ERR_UNKNOWN,
+}sys_err_t;
+
+SYSTEM_RET system_init(void);
+
+#endif /* __APP_system_H */
--- cese_proj/Drivers/APP/Src/APP_system.c
+++ cese_proj/Drivers/APP/Src/APP_system.c
@@ -11,3 +11,40 @@
 #include "APP_system.h"
 #include "API_mcp23s17.h"
 #include "API_lcd.h"
+
+
+static void system_gpio_init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  GPIO_TypeDef *mcp_spi_port = NULL;
+  uint16_t spi_cs_pin = 0;
+  mcp_get_spi_port((void **)&mcp_spi_port);
+  mcp_get_spi_cs_pin(&spi_cs_pin);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(mcp_spi_port, spi_cs_pin, GPIO_PIN_SET);
+  /*Configure GPIO pin : SPI_CS_Pin */
+  GPIO_InitStruct.Pin = spi_cs_pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(mcp_spi_port, &GPIO_InitStruct);
+}
+
+SYSTEM_RET system_init(void)
+{
+	system_gpio_init();
+	lcd_init();
+	mcp_init();
+	return SYS_OK;
+}
